51325165d7b904dcd5f8ce4e2aebca577a6254df
Mark Pude <markpude@gmail.com>
2018-10-11 15:46:26 -0400

    added fail2ban tail

--- starttmux.sh
+++ starttmux.sh
@@ -8,4 +8,8 @@ tmux send-keys "htop" C-m
 tmux splitw -v -p 50
 tmux send-keys "export TERM=\"xterm\"; iftop" C-m
 
+tmux new-window -t "main" -n "fail2ban log"
+tmux send-keys "tail -f /mnt/user/appdata/letsencrypt/log/fail2ban/fail2ban.log" C-m
+
+tmux select-window -t "main:0"
 tmux selectp -t 0
