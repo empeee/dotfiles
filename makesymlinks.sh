5150a6c1bbb24be5ee2de048cc278b096c199097
Mark P ude <markpude@gmail.com>
2022-07-05 19:41:05 -0400

    clean up script and added arguments to select only some compenets to link

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -5,15 +5,32 @@
 ############################
 
 ########## Variables
+dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+olddir=~/.dotfiles_old             # old dotfiles backup directory
+files=""    # list of files/folders to symlink in homedir
+files_tmux="tmux.conf "
+files_vim="vimrc "
+files_bash="bashrc bash_profile bash_prompt bash_aliases "
+files_csh="cshrc "
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="tmux.conf bashrc bash_profile bash_prompt bash_aliases cshrc vimrc dircolors xinitrc"    # list of files/folders to symlink in homedir
+for item in "$@"
+do
+	case $item in
+		tmux)
+			files=$"$files$files_tmux";;
+		vim)
+			files=$"$files$files_vim";;
+		bash)
+			files=$"$files$files_bash";;
+		csh)
+			files=$"$files$files_csh";;
+	esac
+done
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+echo "Creating $olddir for backup of any existing dotfiles"
 mkdir -p $olddir
 echo "...done"
 
@@ -25,7 +42,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
